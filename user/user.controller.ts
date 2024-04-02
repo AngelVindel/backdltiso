9f84ac34d188977fec881cb781e618f954f75842
Juan Labandeira Ros <juanlabandeiraros@gmail.com>
2024-04-02 10:08:37 +0200

    user

---
+++ user/user.controller.ts
@@ -0,0 +1,14 @@
+// user.controller.ts
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return this.userService.signup(createUserDto);
+  }
+}
---
+++ user/user.interface.ts
@@ -0,0 +1,8 @@
+// user.interface.ts
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+  }
+  
