738e58d83a6b53799b47aef095f9b1983635d85f
javierdm01 <diazmolinajavier04@gmail.com>
2024-04-16 08:44:21 +0200

    Fix question error

--- src/questions/questions.entity.ts
+++ src/questions/questions.entity.ts
@@ -1,17 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { Answer } from "src/answers/answers.entity";
-import { Entity, OneToMany } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity()
 export class Question{
 
-    
+    @PrimaryGeneratedColumn()
     id:number;
-
+    @Column()
     text:string;
-
+    @Column()
     description:string;
-
+    
     @OneToMany(()=>Answer, answer=> answer.questionId)
     answers: Answer[];
     
