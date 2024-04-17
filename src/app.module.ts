e9086d364ff64d03ded551b73d8449f913b77873
Juan Labandeira Ros <juanlabandeiraros@gmail.com>
2024-04-17 08:16:13 +0200

    Connection to database modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,11 @@ import { ResendModule } from './email/email.module';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST || 'localhost',
-      port: parseInt(process.env.MYSQL_PORT) || 3306,
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      host:  'localhost',
+      port:  3306,
+      username: 'root',
+      password: '1234abc',
+      database: 'prueba2',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // No usar en producción
     }),
