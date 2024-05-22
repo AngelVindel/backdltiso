e04c6d45b0b80fdd0a320b761f848b4668f968d0
2miguelsuarez <mmiguelsfdez@gmail.com>
2024-05-22 13:11:34 +0200

    Correción error module

--- src/wordDocument/wordDocu.module.ts
+++ src/wordDocument/wordDocu.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { WordController } from './wordDocu.controller';
 import { WordService } from './wordDocu.service';
 import { WordService2 } from './wordDocu2.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RegularUser } from 'src/user/regularU.entity';
+import { WordDoc } from './wordDocu.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WordDoc,RegularUser])],
   controllers: [WordController],
   providers: [WordService,WordService2],
 })
